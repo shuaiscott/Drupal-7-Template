e6e69e82b4c7936b91da344afba295786291d925
Ephraim <ephraim.sng@gmail.com>
2013-11-05 08:06:27 -0700

    Added Featured Content Region

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -80,6 +80,8 @@ $sidebar_right = render($page['sidebar_right']);
 <div id="content" class="wrapper clearfix <?php print ($sidebar_left && $sidebar_right ? 'two-sidebars' : ($sidebar_left || $sidebar_right ? 'one-sidebar' : '')) ?>" role="main">
 	<?php print render($page['highlighted']); ?>
 	<?php print $breadcrumb; ?>
+	
+	<?php print render($page['feature']); ?> 
 
 	<?php print render($title_prefix); ?>
 	<?php if ($title): ?>
